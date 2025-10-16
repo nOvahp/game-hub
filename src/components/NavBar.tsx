3d13fde300fe0e6521a0f1fffe2328da24f464e5
Amirhossein Parandvash <novahpg1374@gmail.com>
2025-10-16 11:56:16 +0330

    Implement searching

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,11 +3,14 @@ import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 
-const Navbar = () => {
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+const Navbar = ({onSearch}: Props) => {
   return (
     <HStack padding="1%">
       <Image src={logo} boxSize="50px" objectFit="cover" alt="Quantonix Logo" />
-      <SearchInput />
+      <SearchInput onSearch={onSearch}/>
       <ColorModeSwitch />
     </HStack>
   );
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -19,7 +19,13 @@ export interface Game {
 
 
 const useGames = (gameQuery: GameQuery) => {
-  return useData<Game>('/games', {params: {genres: gameQuery.genre?.id, platforms: gameQuery.platform?.id, ordering: gameQuery.sortOrder}}, [gameQuery])
+  return useData<Game>('/games', {params: {
+    genres: gameQuery.genre?.id, 
+    platforms: gameQuery.platform?.id, 
+    ordering: gameQuery.sortOrder,
+    search: gameQuery.searchText}
+  }, 
+  [gameQuery])
 }
 
 export default useGames;
