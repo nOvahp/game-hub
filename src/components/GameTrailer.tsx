be624ef7ed9dfb9c39a9ace19b81341277118829
Amirhossein Parandvash <novahpg1374@gmail.com>
2025-10-23 10:30:24 +0330

    Build game trailer

---
+++ src/components/GameTrailer.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import useTrailers from '../hooks/useTrailers'
+import { Spinner } from '@chakra-ui/react';
+
+interface Props {
+    gameId: number;
+}
+
+const GmeTrailer = ({gameId}: Props) => {
+    const {data, error, isLoading} = useTrailers(gameId)
+
+    if (error) throw error;
+
+    if (isLoading) return <Spinner />
+
+    const first= data?.results[0]
+
+    return first ? (
+      <video src={first.data[480]} poster={first.preview} controls />
+    ) : null;
+  }
+
+export default GmeTrailer
